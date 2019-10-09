5cae24e447c69f7d31c668b4448265b451a77dbc
romil <rogoyal@microsoft.com>
2019-10-09 14:50:46 +0530

    Azure key vault get secret action implementation

---
+++ src/KeyVaultActionParameters.ts
@@ -0,0 +1,17 @@
+import util = require("util");
+import * as core from '@actions/core';
+
+export class KeyVaultActionParameters {
+
+    public keyVaultName: string;
+    public secretsFilter: string;
+    public keyVaultUrl: string;
+
+    public getKeyVaultActionParameters() : KeyVaultActionParameters {
+        this.keyVaultName = core.getInput("keyvault");
+        this.secretsFilter = core.getInput("secrets");
+        var azureKeyVaultDnsSuffix = "vault.azure.net";
+        this.keyVaultUrl = util.format("https://%s.%s", this.keyVaultName, azureKeyVaultDnsSuffix);
+        return this;
+    }
+}
